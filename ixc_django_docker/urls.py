c71085b3faed1fe0cec5637951eeeedf90f4fed1
tpmac1990 <40628696+tpmac1990@users.noreply.github.com>
2024-07-12 12:06:32 +1000

    update url to re_path

--- ixc_django_docker/urls.py
+++ ixc_django_docker/urls.py
@@ -2,38 +2,38 @@ from importlib import import_module
 import os
 
 from django.conf import settings
-from django.conf.urls import include, url
 from django.contrib import admin
 from django.core.exceptions import ImproperlyConfigured
+from django.urls import include, re_path
 from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = [
     # Test error templates.
-    url(r'^404/$', TemplateView.as_view(template_name='404.html')),
-    url(r'^500/$', TemplateView.as_view(template_name='500.html')),
+    re_path(r'^404/$', TemplateView.as_view(template_name='404.html')),
+    re_path(r'^500/$', TemplateView.as_view(template_name='500.html')),
 ]
 
 # Django Admin.
 if 'django.contrib.admin' in settings.INSTALLED_APPS:
     _prefix = settings.ADMIN_URL.strip("/")
     urlpatterns += [
-        url(r'^%s/doc/' % _prefix, include('django.contrib.admindocs.urls')),
-        url(r'^%s/' % _prefix, include(admin.site.urls)),
+        re_path(r'^%s/doc/' % _prefix, include('django.contrib.admindocs.urls')),
+        re_path(r"^%s/" % _prefix, admin.site.urls),
     ]
 
 # Django Auth.
 if 'django.contrib.auth' in settings.INSTALLED_APPS:
     urlpatterns += [
-        url(r'^accounts/', include('django.contrib.auth.urls')),
+        re_path(r'^accounts/', include('django.contrib.auth.urls')),
     ]
 
 # Django Debug Toolbar.
 if 'debug_toolbar' in settings.INSTALLED_APPS:
     import debug_toolbar
     urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        re_path(r'^__debug__/', include(debug_toolbar.urls)),
     ]
 
 # Auto-include `project` URLs if they are available
@@ -55,5 +55,5 @@ else:
         'No project urlconf found. Checked: ' + ', '.join(checked)
     )
 urlpatterns += [
-    url(r'^', include(project_urlconf)),
+    re_path(r'^', include(project_urlconf)),
 ]
